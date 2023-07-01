38f2ab21be2fe9e4cb43b7180ad71b587d8ada7d
inga <inga@qupaya.com>
2023-07-01 19:39:03 +0200

    feat: add basic code

---
+++ scene.js
@@ -0,0 +1,53 @@
+const weather = ['sun', ['sun', 'clouds'], ['sun', 'wind'], 'rain', ['rain', 'wind'], ['rain', 'strom'], 'wind', 'clouds', 'snow', 'fog', ['fog', 'sun'], 'storm'];
+const landscape = ['forest', 'lake', 'sea', 'mountain', 'meadows', 'beach', 'wetland'];
+const daytime = ['morning', 'midday', 'afternoon', 'evening', 'night'];
+const season = ['spring', 'summer', 'autumn', 'winter'];
+
+const button = document.getElementById('randomButton');
+button.addEventListener('click', generateRandomEntries);
+
+function generateRandomEntries() {
+    const randomLandscapeIndex = Math.floor(Math.random() * landscape.length);
+    const randomDaytimeIndex = Math.floor(Math.random() * daytime.length);
+    const randomSeasonIndex = Math.floor(Math.random() * season.length);
+    let randomWeatherIndex;
+
+    let randomWeather;
+    const randomLandscape = landscape[randomLandscapeIndex];
+    const randomDaytime = daytime[randomDaytimeIndex];
+    const randomSeason = season[randomSeasonIndex];
+
+    if (season[randomSeasonIndex] === 'winter') {
+        const filteredWinterWeather = weather.filter((condition) => condition !== 'rain');
+        randomWeatherIndex = Math.floor(Math.random() * filteredWinterWeather.length);
+        randomWeather = filteredWinterWeather[randomWeatherIndex];
+    } else {
+        const filteredWeather = weather.filter((condition) => condition !== 'snow');
+        randomWeatherIndex = Math.floor(Math.random() * filteredWeather.length);
+        randomWeather = filteredWeather[randomWeatherIndex];
+    }
+
+
+    const randomWeatherElement = document.getElementById('randomWeather');
+    randomWeatherElement.textContent = randomWeather;
+
+ const randomLandscapeElement = document.getElementById('randomLandscape');
+    randomLandscapeElement.textContent = randomLandscape;
+
+ const randomDaytimeElement = document.getElementById('randomDaytime');
+    randomDaytimeElement.textContent = randomDaytime;
+
+    const randomSeasonElement = document.getElementById('randomSeason');
+    randomSeasonElement.textContent = randomSeason;
+}
+
+/*
+shorter way to do it:
+
+const generateRandomEntries = () => {
+    const randomDaytime = daytime[Math.floor(Math.random() * daytime.length)];
+    const randomSeason = season[Math.floor(Math.random() * season.length)];
+
+    document.getElementById('randomDaytime').textContent = randomDaytime;
+    document.getElementById('randomSeason').textContent = randomSeason;
+};*/
